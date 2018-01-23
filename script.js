7d0074fc98616979b708f9c93d905995e00865cf
Ashley Marku <ashley.marku@lc.cuny.edu>
2018-01-23 15:28:06 -0500

    added comments

--- script.js
+++ script.js
@@ -5,24 +5,29 @@ var fs = require('fs');
 var ejs = require('ejs');
 
 var url = bodyParser.urlencoded({extended:false});
+
+//reads the JSON files information
 var data = fs.readFileSync('loginDatabase.json');
+//postArr is extracting the incoming input and writing it to the JSON file
 var postArr = JSON.parse(data);
 
 var app = express();
 
-app.use(express.static("views"))
+//looks at the views folder thats static and everything to reference from
+app.use(express.static("views")) 
 
+//reads the EJS template
 app.set('view engine', 'ejs')
 
-
+// shows the html within the ejs file at the localhost
 app.get('/quiz', function(require, response){
-	response.render('index.ejs', {'loginDatabase.json': require.loginInfo});
-
-
+	response.render('index.ejs');
 
 });
 
 
+// handles user input and pushes the data into the obj array and writing it 
+// to the JSON file as a string
 
 app.post('/quiz/add/', url, function(request, response){
 
@@ -43,11 +48,7 @@ app.post('/quiz/add/', url, function(request, response){
 })
 
 
-
-app.use(function(req, res, next){
-    res.redirect('/quiz');
-})
-
-app.listen(8080);
+// our local host port, displays "listening" in the terminal to ensure its working
+app.listen(3000);
 console.log("listening");
 
