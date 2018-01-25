94c602dd58a008fb2d1a8177a71ae73bad970018
Mark Ammari <mark.ammari@gmail.com>
2018-01-25 12:01:08 -0500

    adjusted api, i still need to add questions and more buttons

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var btn = document.querySelector("#start-quiz");
+var catagory = document.querySelector('#catagory');
+var type = document.querySelector('#type');
+var difficulty = document.querySelector('#difficulty');
+var question = document.querySelector('#question');
+var multiplechoice = document.querySelector('#multiplechoice');
 
 //listen for clicks
 btn.addEventListener("click", function(){
@@ -8,6 +13,10 @@ btn.addEventListener("click", function(){
   XHR.onreadystatechange = function(){
     if(XHR.readyState == 4 && XHR.status == 200) {
       var data = JSON.parse(XHR.responseText);
+	 catagory.innerHTML = 'Category: ' + data.results[0].category;
+	 type.innerHTML = 'Type: ' + data.results[0].type;
+	 difficulty.innerHTML = 'Difficulty: ' + data.results[0].difficulty;
+	 question.innerHTML = 'Question: ' + data.results[0].question;
     }
   }
   console.log('getting api...');
@@ -15,15 +24,20 @@ btn.addEventListener("click", function(){
   XHR.send();
 });
 
+
+
 $(document).ready(function(){
    
 	$('#start-quiz').click(function(){
 		$('button').fadeOut(3000);
 		$('#board').fadeIn(3000);
+		$('#catagory').fadeIn(6000);
 	});
 
-
-
-
 });
+
+
+
+
+
 
