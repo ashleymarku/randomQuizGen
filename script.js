4d80916ec185f0b088a2f9e595792f959b294c1d
Mark Ammari <mark.ammari@gmail.com>
2018-01-26 10:48:11 -0500

    replace start quiz button with next and could only generate new questions from the first array

--- script.js
+++ script.js
@@ -9,39 +9,36 @@ var a1 = document.querySelector('#a')
 var b2 = document.querySelector('#b')
 var c3 = document.querySelector('#c')
 var d4 = document.querySelector('#d')
-var next = document.querySelector('#next')
 //listen for clicks
 btn.addEventListener("click", function(){
   //make the request
-  var XHR = new XMLHttpRequest();
- 
+  var XHR = new XMLHttpRequest()
+
   XHR.onreadystatechange = function(){
     if(XHR.readyState == 4 && XHR.status == 200) {
           var data = JSON.parse(XHR.responseText);
-	 	for(var i = 0; i < 10; i++){
-	 catagory.innerHTML = 'Category: ' + data.results[i].category;
-	 type.innerHTML = 'Type: ' + data.results[i].type;
-	 difficulty.innerHTML = 'Difficulty: ' + data.results[i].difficulty;
-	 question.innerHTML = 'Question: ' + data.results[i].question
-	 console.log(data.results[i].correct_answer);
-     console.log(data.results[i].incorrect_answers[2]);
-     console.log(data.results[i].incorrect_answers[0]);
-     console.log(data.results[i].incorrect_answers[1]);
-		
+	 	  	
+          	for(var i = 0; i < 10; i++){
+	 	  	catagory.innerHTML = 'Category: ' + data.results[i].category;
+	 	  	type.innerHTML = 'Type: ' + data.results[i].type;
+	 		difficulty.innerHTML = 'Difficulty: ' + data.results[i].difficulty;
+	 		question.innerHTML = 'Question: ' + data.results[i].question
+	 // console.log(data.results[i].correct_answer);
+  //    console.log(data.results[i].incorrect_answers[2]);
+  //    console.log(data.results[i].incorrect_answers[0]);
+  //    console.log(data.results[i].incorrect_answers[1]);
     a1.innerHTML = data.results[i].correct_answer;
     b2.innerHTML = data.results[i].incorrect_answers[2];
 	c3.innerHTML = data.results[i].incorrect_answers[0]; 
-    d4.innerHTML = data.results[i].incorrect_answers[1];
-    	
+    d4.innerHTML = data.results[i].incorrect_answers[1]
     	}
-    		
-
     		}
 		}		
 
   console.log('getting api...');
   XHR.open("GET", 'https://opentdb.com/api.php?amount=10');
   XHR.send();
+
 });
 
 
@@ -49,35 +46,24 @@ btn.addEventListener("click", function(){
 $(document).ready(function(){
    
 	$('#start-quiz').click(function(){
-		$('#start-quiz').fadeOut(3000);
 		$('#board').fadeIn(3000);
 		
-
+	
 	});
 
 	$('.btn1').click(function(){
 	
 			alert('correct!');
-			$('#next').fadeIn(2000);
-			$('.btn2').hide('fast');
+			
+			
 		
 	})
 
 	$('.btn2').click(function(){
-		flag = true;
-		if((b2.innerHTML || c3.innerHTML || d4.innerHTML)){
-			alert('WRONG!')
-			$('.btn2').hide('fast');
-		}
 
-	$('#next').click(function(){
-	 
-	})	
+			alert('WRONG!')
 
 	})
 
-	
-
-	
 
 });
