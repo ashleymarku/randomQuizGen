19ca8a4177117d39871a141eced8c3ca69bb0116
Mark Ammari <mark.ammari@gmail.com>
2018-01-25 22:28:14 -0500

    added next button, still need the next button to generate next question

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var btn = document.querySelector("#start-quiz");
 var catagory = document.querySelector('#catagory');
 var type = document.querySelector('#type');
@@ -8,33 +9,36 @@ var a1 = document.querySelector('#a')
 var b2 = document.querySelector('#b')
 var c3 = document.querySelector('#c')
 var d4 = document.querySelector('#d')
+var next = document.querySelector('#next')
 //listen for clicks
 btn.addEventListener("click", function(){
   //make the request
   var XHR = new XMLHttpRequest();
-  
+ 
   XHR.onreadystatechange = function(){
     if(XHR.readyState == 4 && XHR.status == 200) {
-      var data = JSON.parse(XHR.responseText);
-	 catagory.innerHTML = 'Category: ' + data.results[0].category;
-	 type.innerHTML = 'Type: ' + data.results[0].type;
-	 difficulty.innerHTML = 'Difficulty: ' + data.results[0].difficulty;
-	 question.innerHTML = 'Question: ' + data.results[0].question
-     console.log(data.results[0].correct_answer)
-     console.log(data.results[0].incorrect_answers[2]);
-     console.log(data.results[0].incorrect_answers[0]);
-     console.log(data.results[0].incorrect_answers[1]);
+          var data = JSON.parse(XHR.responseText);
+	 	for(var i = 0; i < 10; i++){
+	 catagory.innerHTML = 'Category: ' + data.results[i].category;
+	 type.innerHTML = 'Type: ' + data.results[i].type;
+	 difficulty.innerHTML = 'Difficulty: ' + data.results[i].difficulty;
+	 question.innerHTML = 'Question: ' + data.results[i].question
+	 console.log(data.results[i].correct_answer);
+     console.log(data.results[i].incorrect_answers[2]);
+     console.log(data.results[i].incorrect_answers[0]);
+     console.log(data.results[i].incorrect_answers[1]);
+		
+    a1.innerHTML = data.results[i].correct_answer;
+    b2.innerHTML = data.results[i].incorrect_answers[2];
+	c3.innerHTML = data.results[i].incorrect_answers[0]; 
+    d4.innerHTML = data.results[i].incorrect_answers[1];
+    	
+    	}
+    		
 
-    var arr = [a1.innerHTML = data.results[0].correct_answer,
-     b2.innerHTML = data.results[0].incorrect_answers[2],
-     c3.innerHTML = data.results[0].incorrect_answers[0],
-     d4.innerHTML = data.results[0].incorrect_answers[1]];
+    		}
+		}		
 
-     
-     console.log(arr.shuffle());
-
-    }
-  }
   console.log('getting api...');
   XHR.open("GET", 'https://opentdb.com/api.php?amount=10');
   XHR.send();
@@ -47,26 +51,33 @@ $(document).ready(function(){
 	$('#start-quiz').click(function(){
 		$('#start-quiz').fadeOut(3000);
 		$('#board').fadeIn(3000);
-		$('#catagory').fadeIn(6000);
+		
+
 	});
 
+	$('.btn1').click(function(){
+	
+			alert('correct!');
+			$('#next').fadeIn(2000);
+			$('.btn2').hide('fast');
+		
+	})
+
+	$('.btn2').click(function(){
+		flag = true;
+		if((b2.innerHTML || c3.innerHTML || d4.innerHTML)){
+			alert('WRONG!')
+			$('.btn2').hide('fast');
+		}
+
+	$('#next').click(function(){
+	 
+	})	
+
+	})
+
+	
+
+	
+
 });
-
-Array.prototype.shuffle = function() {
-   var input = this;
-   
-   for (var i = input.length-1; i >=0; i--) {
-   
-       var randomIndex = Math.floor(Math.random()*(i+1));
-       var itemAtIndex = input[randomIndex];
-       
-       input[randomIndex] = input[i];
-       input[i] = itemAtIndex;
-   }
-   return input;
-}
-
-
-
-
-
